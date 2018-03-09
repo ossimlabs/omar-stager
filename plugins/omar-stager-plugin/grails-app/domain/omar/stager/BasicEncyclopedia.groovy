49173781f0c570542b49b3838294a4f406a3e0b2
knaquin <kevin.naquin@digitalglobe.com>
2018-03-09 14:19:51 -0500

    Adding BE service.

---
+++ plugins/omar-stager-plugin/grails-app/domain/omar/stager/BasicEncyclopedia.groovy
@@ -0,0 +1,59 @@
+package omar.stager
+
+
+import com.vividsolutions.jts.geom.Point
+
+
+class BasicEncyclopedia {
+
+    String activity
+    String affiliation
+    String be
+    String category
+    String cc
+    String classification
+    String codeWord
+    String condition
+    String dateLastChanged
+    String domainLv1
+    String lastUpdated
+    Point location
+    String msnPrimary
+    String name
+    String polSubdivision
+    String recordStatus
+    String releaseMark
+    String reviewDate
+    String sk
+    String suffix
+    String symbolId
+
+
+    // only be, location and suffix are required
+    static constraints = {
+        activity nullable: true
+        affiliation nullable: true
+        category nullable: true
+        cc nullable: true
+        classification nullable: true
+        codeWord nullable: true
+        condition nullable: true
+        dateLastChanged nullable: true
+        domainLv1 nullable: true
+        lastUpdated nullable: true
+        msnPrimary nullable: true
+        name nullable: true
+        polSubdivision nullable: true
+        recordStatus nullable: true
+        releaseMark nullable: true
+        reviewDate nullable: true
+        sk nullable: true
+        suffix nullable: true
+        symbolId nullable: true
+    }
+
+    static mapping = {
+        be index: "basic_encyclopedia_be_idx"
+        location sqlType: "geometry(Point, 4326)"
+    }
+}
---
+++ plugins/omar-stager-plugin/grails-app/services/omar/stager/BeService.groovy
@@ -0,0 +1,85 @@
+package omar.stager
+
+
+import groovy.json.JsonSlurper
+import com.vividsolutions.jts.io.WKTReader
+
+
+class BeService {
+
+	def grailsApplication
+
+
+	def getBeInfo( be ) {
+		def url = "${ grailsApplication.config.beInfoUrl }&BE=${ be }"
+		def text = url.getText()
+		def json = new JsonSlurper().parseText( text )
+
+
+		return json
+	}
+
+	def saveBeInfo( be, info ) {
+		be.activity = info.activity
+		be.affiliation = info.affiliation
+		be.be = info.be
+		be.category = info.category
+		be.cc = info.cc
+		be.classification = info.classification
+		be.codeWord = info.codeWord
+		be.condition = info.condition
+		be.dateLastChanged = info.dateLastChanged
+		be.domainLv1 = info.domainLv1
+		be.lastUpdated = info.lastUpdated
+		be.location = info.location
+		be.msnPrimary = info.msnPrimary
+		be.name = info.name
+		be.polSubdivision = info.pol_subdiv
+		be.recordStatus = info.recordStatus
+		be.releaseMark = info.releaseMark
+		be.reviewDate = info.reviewDate
+		be.sk = info.SK
+		be.suffix = info.suffix
+		be.symbolId = info.symbolid
+
+		be.save()
+		if ( be.hasErrors() ) {
+			be.errors.allErrors.each { println it }
+		}
+
+
+		return
+	}
+
+	def updateBeTable() { //( beNumber, overwrite ) {
+		def bes = BasicEncyclopedia.findAllByBeLike( "%${ beNumber }%" )
+
+		// if no BEs exists, make a new entry
+		if ( bes.size() < 1 ) {
+			def beInfo = getBeInfo( beNumber )
+			beInfo.features.each {
+				def be = it
+				def info = be.properties
+
+				def location = be.geometry.coordinates
+				info.location = new WKTReader().read( "POINT( ${ location.join( " " ) } )" )
+
+				saveBeInfo( new BasicEncyclopedia(), info )
+			}
+		}
+		// if a force overwrite is requested
+		else if ( bes.size() > 0 && overwrite ) {
+			def beInfo = getBeInfo( beNumber )
+			beInfo.features.each {
+				def suffix = it.suffix
+				def be = bes.find { it.suffix = suffix }
+				def info = be.properties
+
+				saveBeInfo( be, info )
+			}
+		}
+
+
+		return
+	}
+}

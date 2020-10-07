27a41e1462117fbb9070f5b6399a559e76bf840b
Craig Schneider <craig.schneider@maxar.com>
2020-10-07 12:40:42 -0400

    refactor try-catch

--- apps/omar-stager-app/grails-app/init/omar/stager/app/BootStrap.groovy
+++ apps/omar-stager-app/grails-app/init/omar/stager/app/BootStrap.groovy
@@ -3,6 +3,7 @@ package omar.stager.app
 import grails.util.Holders
 import groovy.util.logging.Slf4j
 import omar.core.Repository
+import omar.raster.RasterEntry
 import omar.stager.core.StagerJob
 import org.quartz.TriggerKey
 
@@ -15,47 +16,52 @@ class BootStrap {
 
     def init = { servletContext ->
         try {
-            if (Environment.current == Environment.DEVELOPMENT) {
-                [
-                        // '/Volumes/Iomega_HDD/data',
-                        // '/data/celtic',
-                        // '/data1',
-                        // '/data/uav'
-                ].each {
-                    println it
-                    def repo = Repository.findOrCreateByBaseDir(it)
-                    repo.save()
-                    StagerJob.triggerNow(baseDir: repo.baseDir)
-                }
-                // sessionFactory?.currentSession?.flush()
-            }
-
-            grailsApplication = Holders.grailsApplication
-            //StagerConfig.application = grailsApplication
-            //StagerUtils.resetSqsConfig()
-            //StagerUtils.sqsConfig
-
-            def quartzScheduler = grailsApplication.mainContext.getBean('quartzScheduler')
-
-            //if(SqsUtils.sqsConfig.reader.enabled)
-            //{
-            org.quartz.TriggerKey triggerKey = new TriggerKey("StageFileJobTrigger", "StageFileJobGroup")
-
-            def trigger = quartzScheduler.getTrigger(triggerKey)
-            if (trigger) {
-                trigger.repeatInterval = 5000l
-                // trigger.repeatInterval = StagerUtils.sqsConfig.reader.pollingIntervalSeconds*1000 as Long
-
-                Date nextFireTime = quartzScheduler.rescheduleJob(triggerKey, trigger)
-            }
-            //}
+            // Hack for assisting with improper database setup!
+            // If there is an exception accessing the raster_entry table, provide possible causes.
+            RasterEntry.count()
         }
         catch (final Exception e) {
             log.error("Bootstrap init failure. If the exception is from Hibernate unable to create raster_entry, " +
                     "the likely cause is missing postgis extensions/schemas. Omar Stager uses the Omar Raster plugin " +
                     "which requires postgis DB schema.")
             log.error(e.message)
+            throw(e)
         }
+        if (Environment.current == Environment.DEVELOPMENT) {
+            [
+                    // '/Volumes/Iomega_HDD/data',
+                    // '/data/celtic',
+                    // '/data1',
+                    // '/data/uav'
+            ].each {
+                println it
+                def repo = Repository.findOrCreateByBaseDir(it)
+                repo.save()
+                StagerJob.triggerNow(baseDir: repo.baseDir)
+            }
+            // sessionFactory?.currentSession?.flush()
+        }
+
+        grailsApplication = Holders.grailsApplication
+        //StagerConfig.application = grailsApplication
+        //StagerUtils.resetSqsConfig()
+        //StagerUtils.sqsConfig
+
+        def quartzScheduler = grailsApplication.mainContext.getBean('quartzScheduler')
+
+        //if(SqsUtils.sqsConfig.reader.enabled)
+        //{
+        org.quartz.TriggerKey triggerKey = new TriggerKey("StageFileJobTrigger", "StageFileJobGroup")
+
+        def trigger = quartzScheduler.getTrigger(triggerKey)
+        if (trigger) {
+            trigger.repeatInterval = 5000l
+            // trigger.repeatInterval = StagerUtils.sqsConfig.reader.pollingIntervalSeconds*1000 as Long
+
+            Date nextFireTime = quartzScheduler.rescheduleJob(triggerKey, trigger)
+        }
+        //}
+
     }
     def destroy = {
     }

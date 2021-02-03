fff6a82fb854deda29d4004559d4eaed99c4061d
jzuehsow <57189589+jzuehsow@users.noreply.github.com>
2021-02-03 09:13:06 -0500

    Update _TEST.js

---
+++ cypress/integration/examples/_TEST.js
@@ -0,0 +1,9 @@
+// GSP-4101 Tests
+describe('GSP-4101 mission_id runtime tests'), () => {
+    context('context'), () => {
+        expect(add(1, 2)).to.eq(3)
+    }
+
+    it()
+    cy.request('GET', url, body)
+}

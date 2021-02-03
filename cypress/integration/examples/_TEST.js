6981535555f2268b8eae06012190b5d49f591379
jzuehsow <57189589+jzuehsow@users.noreply.github.com>
2021-02-03 14:59:35 -0500

    Update _TEST.js

--- cypress/integration/examples/_TEST.js
+++ cypress/integration/examples/_TEST.js
@@ -1,9 +1,24 @@
 // GSP-4101 Tests
-describe('GSP-4101 mission_id runtime tests'), () => {
-    context('context'), () => {
-        expect(add(1, 2)).to.eq(3)
-    }
-
-    it()
-    cy.request('GET', url, body)
-}
+describe('GSP-4101 mission_id runtime tests', () => {
+    /*context('context'), () => {
+        expect(add(1, 2)).to.eq(3)*/
+/*
+    it('DESCRIPTION', () => {
+        cy
+        .request('GET', 'http://localhost:8888/users/admin')
+        .then((response) => {
+            // response.body is automatically serialized into JSON
+            expect(response.body).to.have.property('name', 'Jane') // true
+        })    
+    })
+*/
+    let features
+it('Grab up to 1000 images', () => {
+    cy.request({method: "GET", url: "/wfs?filter=&outputFormat=JSON&request=GetFeature&service=WFS&sortBy=acquisition_date+D&startIndex=0&typeName=omar:raster_entry&version=1.1.0"})
+        .then((response) => {
+            expect(response.body).to.have.property('type', 'FeatureCollection')
+            features = response.body.features
+        })
+})
+    
+})

0d848273859f339bc1d87d5fc1e7c6b9d8e1e2c2
Jeffrey Pavlov <jeffreypavlov@Jeffreys-MacBook-Pro.local>
2021-02-16 17:19:46 -0500

    Retrieving response from Url using the cypress

--- cypress/integration/APITesting.js
+++ cypress/integration/APITesting.js
@@ -1,76 +1,28 @@
-let json = require('../fixtures/testParameters.json');
-let tests = Object.keys(json.tests);
-let innerJson, method, endpoint, inValue, good, parameters, request, keys;
 
-describe('Automated tests for the omar-superoverlay API endpoints', () => {
-    tests.forEach((test) => {
-        it(`Should test ${test}`, () => {
-            innerJson = json.tests[test]
-            method = innerJson["method"];
-            endpoint = innerJson["endpoint"];
-            inValue = innerJson["in"];
-            good = innerJson["expected"] === "good";
-            parameters = Object.keys(innerJson["parameters"]);
-            if(inValue === "query" && good) {
-                request = "?"
-                parameters.forEach((parameter) => {
-                    request = request + parameter + "=" + innerJson.parameters[parameter] + "&";
-                })
-                request = request.substring(0, request.length - 1);
-                cy.request({method: method, url: endpoint + request, failOnStatusCode: false})
-                    .then((response) => {
-                        expect(response.status).to.eq(200)
-                    })
-            }
-            else if(inValue === "query") {
-                request = "?"
-                parameters.forEach((parameter) => {
-                    request = request + parameter + "=" + innerJson.parameters[parameter] + "&";
-                })
-                request = request.substring(0, request.length - 1);
-                cy.request({method: method, url: endpoint + request, failOnStatusCode: false})
-                    .then((response) => {
-                        expect(response.status).to.not.eq(200)
-                    })
-            }
-            else if(inValue === "body") {
-                cy.request({method: method, url: endpoint, body: innerJson.parameters["body"], failOnStatusCode: false})
-                    .then((response) => {
-                        expect(response.status).to.eq(200)
-                    })
-            }
-            else {
-                cy.request({method: method, url: endpoint + "/" + innerJson.parameters["id"], failOnStatusCode: false})
-                    .then((response) => {
-                        expect(response.status).to.eq(200)
-                    })
-            }
-        })
-        it(`Should test the header of ${test}`, () => {
-            innerJson = json.tests[test]
-            method = innerJson["method"];
-            endpoint = innerJson["endpoint"];
-            inValue = innerJson["in"];
-            good = innerJson["expected"] === "good";
-            parameters = Object.keys(innerJson["parameters"]);
-            if(inValue === "query") {
-                cy.request({method: method, url: endpoint + request, failOnStatusCode: false})
-                    .then((response) => {
-                        expect(response).to.have.property("headers")
-                    })
-            }
-            else if(inValue === "body") {
-                cy.request({method: method, url: endpoint, body: innerJson.parameters["body"], failOnStatusCode: false})
-                    .then((response) => {
-                        expect(response).to.have.property("headers")
-                    })
-            }
-            else {
-                cy.request({method: method, url: endpoint + "/" + innerJson.parameters["id"], failOnStatusCode: false})
-                    .then((response) => {
-                        expect(response).to.have.property("headers")
-                    })
-            }
+// Declare the variables
+let allImages;
+
+// Get the json here, store in variable
+
+// McKenzie - We got our collection ;)
+describe('yet another test', () => {
+    it('here we go again', () => {
+       // const url = https://omar-dev.ossim.io/omar-wfs/wfs?service=WFS&version=1.1.0&request=GetFeature&typeName=omar:raster_entry&filter=&outputFormat=JSON&sortBy=mission_id+D&startIndex=0;
+        cy.request({
+            method: 'Get',
+            url: `https://omar-dev.ossim.io/omar-wfs/wfs?service=WFS&version=1.1.0&request=GetFeature&typeName=omar:raster_entry&filter=&outputFormat=JSON&sortBy=mission_id+D&startIndex=0`,
+        }).then((response) => {
+            console.log('response', response)
+            console.log('responseBody', response.body)
+            // expect(response).property('status').to.equal(200)
+            // expect(response.body).property('id').to.not.be.oneOf([null, ""])
+            const body = (response.body)
+
         })
     })
 })
+
+// iterate through and grab the ones with mission id and filename
+
+
+// Make a call to the endpoint to get JSON data

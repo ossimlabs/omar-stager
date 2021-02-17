0349be833fca524daeb252810ff27145b908ab52
Jeffrey Pavlov <jeffreypavlov@Jeffreys-MacBook-Pro.local>
2021-02-17 10:21:32 -0500

    with Aimee changs

--- cypress/integration/APITesting.js
+++ cypress/integration/APITesting.js
@@ -1,7 +1,7 @@
 
 // Declare the variables
 let fullCollection;
-
+let allMissionIds;
 // Get the json here, store in variable
 
 
@@ -9,23 +9,53 @@ let fullCollection;
 
 // McKenzie - We got our collection ;)
 describe('yet another test', () => {
-    it('here we go again', () => {
-       // const url = https://omar-dev.ossim.io/omar-wfs/wfs?service=WFS&version=1.1.0&request=GetFeature&typeName=omar:raster_entry&filter=&outputFormat=JSON&sortBy=mission_id+D&startIndex=0;
-        cy.request({
-            method: 'Get',
-            url: `https://omar-dev.ossim.io/omar-wfs/wfs?service=WFS&version=1.1.0&request=GetFeature&typeName=omar:raster_entry&filter=&outputFormat=JSON&sortBy=mission_id+D&startIndex=0`,
-        }).then((response) => {
-           // console.log('response', response)
-           // console.log('responseBody', response.body)
-            fullCollection = response.body;
-            for(let i=0; i<= fullCollection.features.length; i++){
-               console.log(fullCollection[i]);
-            }
-            // expect(response).property('status').to.equal(200)
-            // expect(response.body).property('id').to.not.be.oneOf([null, ""])
 
+    it('Grab all mission ids', () => {
+        cy.request({
+            method: "GET",
+            url: `https://omar-dev.ossim.io/omar-stager/dataManager/getDistinctValues?property=missionId`
+        }).then((response) =>{
+            console.log('all mission ids: ', response.body)
+            allMissionIds = response.body;
         })
     })
+
+    it('Loop through and grab 3 of each mission ids', ()=> {
+        allMissionIds.forEach((id) => {
+            cy.request({method: "GET", url: "https://omar-dev.ossim.io/omar-wfs/wfs?filter=mission_id%20LIKE%20%27%25"+id+"%25%27&outputFormat=JSON&request=GetFeature&service=WFS&sortBy=acquisition_date%2BD&startIndex=0&typeName=omar%3Araster_entry&version=1.1.0"})
+                .then((response) => {
+                    let temp
+                    console.log(response.body)
+                    for(var i = 0; i < response.body.totalFeatures; i++) {
+                        temp = response.body.features[i].properties['mission_id']
+                        expect(temp).to.eq(id)
+                    }
+                })
+        })
+    })
+
+
+
+
+    //
+    // it('here we go again', () => {
+    //    // const url = https://omar-dev.ossim.io/omar-wfs/wfs?service=WFS&version=1.1.0&request=GetFeature&typeName=omar:raster_entry&filter=&outputFormat=JSON&sortBy=mission_id+D&startIndex=0;
+    //     cy.request({
+    //         method: 'Get',
+    //         url: `https://omar-dev.ossim.io/omar-wfs/wfs?maxFeatures=3&filter=mission_id%20IS%20LIKE%20COSMO-SkyMed&outputFormat=JSON&request=GetFeature&service=WFS&sortBy=acquisition_date+D&startIndex=0&typeName=omar:raster_entry&version=1.1.0`,
+    //     }).then((response) => {
+    //        // console.log('response', response)
+    //        // console.log('responseBody', response.body)
+    //         fullCollection = response.body;
+    //         console.log(fullCollection.length)
+    //         for(let i=0; i<= fullCollection.features.length; i++){
+    //            console.log(fullCollection[i]);
+    //         }
+    //         // expect(response).property('status').to.equal(200)
+    //         // expect(response.body).property('id').to.not.be.oneOf([null, ""])
+    //
+    //     })
+    // })
 })
 
 // iterate through and grab the ones with mission id and filename
